3973628890db77a2eeded13f7991fedbb6872896
HashaamMasood-97 <hashammasood@gmail.com>
2020-07-02 22:00:18 +0500

    firebase-react-crud

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase"
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBrCEWbVcunWjK_GZ7fgQnuQo9Gv10uEOY",
+    authDomain: "react-crud-7ca4e.firebaseapp.com",
+    databaseURL: "https://react-crud-7ca4e.firebaseio.com",
+    projectId: "react-crud-7ca4e",
+    storageBucket: "react-crud-7ca4e.appspot.com",
+    messagingSenderId: "582561820943",
+    appId: "1:582561820943:web:8e0f0ff653a391efd28e7a"
+  };
+  // Initialize Firebase
+ var fireDb= firebase.initializeApp(firebaseConfig);
+
+
+ export default fireDb.database().ref();
+
+
+  
