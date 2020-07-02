bc8d37b3390ce4ae44c0fec7280bea870e14be8b
HashaamMasood-97 <hashammasood@gmail.com>
2020-07-03 00:57:15 +0500

    extra

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export class Header extends Component {
         return (
             <div>
                 <h1>Welcome to firebase with react</h1>
-                <li><a href="http://localhost:3000/a">Hook</a></li>
+                <li><a href="http://localhost:3000/a">Hooks</a></li>
             <li><a href="http://localhost:3000/b">Simple</a></li>
             </div>
         )
